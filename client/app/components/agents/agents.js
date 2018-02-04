530067fac4d37a24c9f15b9cff492f90b155533e
Christoph Achsenick <christoph@achsenick.de>
2018-02-04 12:49:46 +1100

    - added router config for agents component - import agents component to components module

--- client/app/components/agents/agents.js
+++ client/app/components/agents/agents.js
@@ -6,6 +6,15 @@ let agentsModule = angular.module('agents', [
   uiRouter
 ])
 
+.config(($stateProvider) => {
+  "ngInject";
+  $stateProvider
+    .state('agents', {
+      url: '/agents',
+      component: 'agents'
+    });
+})
+
 .component('agents', agentsComponent)
 
 .name;
